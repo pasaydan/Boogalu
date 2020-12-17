661da83a48c81835554dde2b8ebdb4a1b3c88615
Vikas Singh <realtyrapport@gmail.com>
2020-12-17 22:09:56 +0530

    Steezy named renamed to boogalu

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import boogaluLogo from '../../Images/Boogalu-logo.svg';
+import './footer.css';
 
 function Footer() {
     return (
         <div>
             <footer className="flex-container-VC">
                 <div className="flex-1 copyright-wrap">
-                    <div><img src="https://global-uploads.webflow.com/5dbb40d6d8c97447e9450447/5e604560e2c5a570b6d92ae1_STEEZY_LOGOMARK.svg" alt="" className="image-7" /></div>
-                    <div>&#169; 2020 STEEZY STUDIO.</div>
+                    <div><img src={boogaluLogo} alt="Logo" className="image-7" /></div>
+                    <div>&#169; 2021 &nbsp;  
+                        <a href="https://www.choreoculture.com/" target="_blank">
+                            CHOREOCULTURE STUDIO.
+                        </a>
+                    </div>
                     <div>ALL RIGHTS RESERVED.</div>
                 </div>
                 <div className="flex-1">
-                    <div className="footer-tagline" >Get the latest dance content & STEEZY news</div>
+                    <div className="footer-tagline" >Get the latest dance content &amp; ChoreoCulture news</div>
                     <div>
                         <input type="email" placeholder="Email" />
                     </div>
