7a52ce255be032f35e7d861661c775b7e44939ff
Danny <garudkardnyaneshwar@gmail.com>
2021-01-26 23:13:11 +0530

    changes for nav

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -149,11 +149,9 @@ function Navigation() {
         );
     }
 
-    if (!didMount) {
-        function navigateToUserRegistrationLogin(path) {
-            toggleUserRegistrationPage(false);
-            history.push(`/${path}`)
-        }
+    function navigateToUserRegistrationLogin(path) {
+        toggleUserRegistrationPage(false);
+        history.push(`/${path}`)
     }
 
     if (!didMount) {
@@ -177,10 +175,11 @@ function Navigation() {
                                 <li><a href="#Subscription" onClick={(e) => onClickNav(e, 'subscription')}>Subscription</a></li>
                             </ul> : ''
                     }
-                    {!state.loggedInUser && state.loggedInUser.phone && <div className="flex-2 signup-wrap" >
+                    {(!state.loggedInUser || !state.loggedInUser.phone) && <div className="flex-2 signup-wrap" >
                         <button className="btn primary-light login" onClick={() => navigateToUserRegistrationLogin('login')}>Login</button>
                         <button className="btn primary-dark signup" onClick={() => navigateToUserRegistrationLogin('register')}>Sign Up</button>
                     </div>}
+
                     {state.loggedInUser && state.loggedInUser.phone && <div className="flex-2 signup-wrap" >
                         <div className="profile" ref={ref}>
                             <AccountCircleOutlinedIcon onClick={() => setShowProfileTab(true)} style={{ fontSize: '35px', paddingRight: '20px' }} />
