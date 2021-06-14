8464fa09718571e3c88b2ec4e95b90f0f9612c05
vikas singh <vicky13vikas@gmail.com>
2021-06-14 22:03:16 +0530

    fix: subscription modal opening for non-logged in user when coming back from login

--- src/Components/Subscriptions/index.js
+++ src/Components/Subscriptions/index.js
@@ -316,7 +316,7 @@ function Subscriptions(props) {
                     </div>
                 </div>
             </div>
-            {showSubscriptionDetails && <BuySubscription
+            {showSubscriptionDetails && loggedInUser.key && <BuySubscription
                 handleClose={() => setShowSubscriptionDetails(false)}
                 activeStep={activeStep}
                 buttonLoadingClass={buttonLoadingClass}
